337569fd8f7dd850bdffbbd5845b6fb0a40a5805
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-08-24 18:58:01 +0530

    Organised File structure, SQL connected

--- init.sql
+++ init.sql
@@ -1,57 +1,49 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
-
 CREATE DATABASE feedback;
 
-GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
-
-USE auth;
+USE feedback;
 
 CREATE TABLE teacher(
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
     subject_id INT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO teacher (email, password) VALUES ('t1', '1');
+INSERT INTO teacher (subject_id, email, password) VALUES (2, 't1', '1');
 
 
 CREATE TABLE student(
-    reg_no INT NOT NULL UNIQUE PRIMARY KEY,
+    reg_no INT NOT NULL UNIQUE PRIMARY KEY
 );
 
 -- insert multiple registeration number into student (reg_no)
 INSERT INTO student (reg_no) VALUES (406), (251), (432), (354), (696), (420), (111);
 
-CREATE TABLE class_feedback
+CREATE TABLE class_feedback(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     subject_id INT NOT NULL,
     reg_no INT NOT NULL UNIQUE,
     q1 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q2 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q3 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q4 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q5 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    FOREIGN KEY (reg_no) REFERENCES student(reg_no)
-    FOREIGN KEY (subject_id) REFERENCES teacher(subject_id)
-;
+    q2 INT NOT NULL CHECK (q2 BETWEEN 1 AND 5),
+    q3 INT NOT NULL CHECK (q3 BETWEEN 1 AND 5),
+    q4 INT NOT NULL CHECK (q4 BETWEEN 1 AND 5),
+    q5 INT NOT NULL CHECK (q5 BETWEEN 1 AND 5)
+);
 
-CREATE TABLE subject_feedback
+CREATE TABLE subject_feedback(
+    subject_id INT NOT NULL PRIMARY KEY,
+    q1 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
+    q2 INT NOT NULL CHECK (q2 BETWEEN 1 AND 5),
+    q3 INT NOT NULL CHECK (q3 BETWEEN 1 AND 5),
+    q4 INT NOT NULL CHECK (q4 BETWEEN 1 AND 5),
+    q5 INT NOT NULL CHECK (q5 BETWEEN 1 AND 5)
+);
+
+CREATE TABLE weekly_feedback(
     subject_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     q1 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q2 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q3 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q4 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q5 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    FOREIGN KEY (subject_id) REFERENCES teacher(subject_id)
-;
-
-CREATE TABLE weekly_feedback
-    subject_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    q1 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q2 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q3 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q4 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    q5 INT NOT NULL CHECK (q1 BETWEEN 1 AND 5),
-    FOREIGN KEY (subject_id) REFERENCES teacher(subject_id)
-;
+    q2 INT NOT NULL CHECK (q2 BETWEEN 1 AND 5),
+    q3 INT NOT NULL CHECK (q3 BETWEEN 1 AND 5),
+    q4 INT NOT NULL CHECK (q4 BETWEEN 1 AND 5),
+    q5 INT NOT NULL CHECK (q5 BETWEEN 1 AND 5)
+);
 
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 #lecture feedback system
 
 import jwt, datetime, os
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, make_response
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 server = Flask(__name__)
 mysql = MySQL(server)
@@ -11,14 +15,14 @@ server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 JWT_SECRET = "humpetohhaihino"
 
 print(os.environ.get("MYSQL_DB"))
 
 @server.route("/login", methods=["GET"])
 def show_login():
-    return render_template("client/views/login.html")
+    return render_template("/views/login.html")
 
 
 @server.route("/login", methods=["POST"])
@@ -26,20 +30,14 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    print(username, password)
-    if(username == "t1" and password == "1"):
-        jwt_token = createJWT(username, JWT_SECRET, True)
-        return redirect(url_for("dashboard", token=jwt_token))
     
-
-    auth = request.authorization
-    if not auth:
+    if username == "":
         return "Missing credentials", 401
     
     #check for username and password
     cur = mysql.connection.cursor()
     res = cur.execute(
-        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+        "SELECT email, password FROM teacher WHERE email=%s", (username,)
     )
 
     if res > 0:
@@ -47,39 +45,41 @@ def login():
         email = user_row[0]
         password = user_row[1]
 
-        if auth.username != email or auth.password != password:
+        if username != email or password != password:
             return "Invalid credentials", 401
         else:
-            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True), render_template("/client/views/dashboard.html", username=auth.username)
+            jwt_token = createJWT(username, JWT_SECRET, True)
+            response = make_response(redirect(url_for("dashboard")))
+            response.set_cookie("jwt_token", jwt_token)
+            return response  
     else:
         return "User not found", 401
 
 
 @server.route("/", methods=["GET"])
 def dashboard():
-    token = request.headers.get("Authorization")
+    token = request.cookies.get("jwt_token")
+    print("JWT Token from Cookie:", token)
     # validate token from validate() function and redirect to dashboard
-    if not token:
+    if token == "":
         return "Missing token", 401
     else:
         # validate
-        decoded = validate()
-        if decoded[1] == 200:
-            return render_template("/client/views/dashboard.html", username=decoded[0]["username"])
+        decoded, status = validate(token)
+        if status == 200:
+            return render_template("/views/dashboard.html", username=decoded["username"])
         else:
-            return decoded[0], 401
+            return decoded, 401
     
 
-def validate():
-    token = request.headers.get("Authorization")
-    if not token:
+def validate(token):
+    # token = request.headers.get("Authorization")
+    if token == "":
         return "Missing token", 401
-
-    encodedJwt = token.split(" ")[1]
 
     try:
         decoded = jwt.decode(
-            encodedJwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+            token, JWT_SECRET, algorithms=["HS256"]
         )
         return decoded, 200
     except:
